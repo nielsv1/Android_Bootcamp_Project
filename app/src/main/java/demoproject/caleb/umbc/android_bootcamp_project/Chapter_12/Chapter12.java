4f9144ac717fdc82a3a39692c34b1a0d8b78f399
calebamassey <43453784+calebamassey@users.noreply.github.com>
2018-12-06 13:59:47 -0500

    Updated Ch6, Ch8, Ch9, Ch12, and MainActivity

--- app/src/main/java/demoproject/caleb/umbc/android_bootcamp_project/Chapter_12/Chapter12.java
+++ app/src/main/java/demoproject/caleb/umbc/android_bootcamp_project/Chapter_12/Chapter12.java
@@ -1,15 +1,15 @@
-package demoproject.caleb.umbc.android_bootcamp_project.Chapter_5.ChocolateCafe;
+package demoproject.caleb.umbc.android_bootcamp_project.Chapter_12;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 import demoproject.caleb.umbc.android_bootcamp_project.R;
 
-public class Mousse extends AppCompatActivity {
+public class Chapter12 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.ch5_chocolatecafe_mousse);
+        setContentView(R.layout.activity_chapter12);
     }
 }
