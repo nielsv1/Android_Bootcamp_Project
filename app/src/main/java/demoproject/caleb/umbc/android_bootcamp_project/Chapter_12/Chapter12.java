fcf9f87f9151fdc9a1938cbe59a3327740cd08bb
nielsv1 <niels.verhoeven11@gmail.com>
2018-12-06 17:35:09 -0500

    Revert "bug fixin"
    This reverts commit 4b85466f26f7da698d2cabdf1becc15b7dc02ceb.

--- app/src/main/java/demoproject/caleb/umbc/android_bootcamp_project/Chapter_12/Chapter12.java
+++ app/src/main/java/demoproject/caleb/umbc/android_bootcamp_project/Chapter_12/Chapter12.java
@@ -1,13 +1,12 @@
 package demoproject.caleb.umbc.android_bootcamp_project.Chapter_12;
 
-import android.app.ListActivity;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.widget.ArrayAdapter;
+import android.os.Bundle;
 
 import demoproject.caleb.umbc.android_bootcamp_project.R;
 
 public class Chapter12 extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
