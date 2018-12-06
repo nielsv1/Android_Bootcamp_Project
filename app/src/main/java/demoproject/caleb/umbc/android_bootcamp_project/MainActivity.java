06c23cf5be4b6ace9cd350fe302905cddd2f8c83
nielsv1 <niels.verhoeven11@gmail.com>
2018-12-06 17:36:51 -0500

    Revert "Revert "bug fixin""
    This reverts commit fcf9f87f9151fdc9a1938cbe59a3327740cd08bb.

--- app/src/main/java/demoproject/caleb/umbc/android_bootcamp_project/MainActivity.java
+++ app/src/main/java/demoproject/caleb/umbc/android_bootcamp_project/MainActivity.java
@@ -25,6 +25,48 @@ public class MainActivity extends ListActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chapter12);
+        String[ ] attraction = {"Chapter 1: Voila! Meet Android", "Chapter 2: Simplify! The Android User Interface", "Chapter 3: Engage! Android User Input, Variables, and Operations", "Chapter 4: Explore! Icons and Decision-Making Controls", "Chapter 5: Investigate! Android Lists, Arrays, and Web Browsers", "Chapter 6: Jam! Implementing Audio in Android Apps", "Chapter 7: Reveal! Displaying Pictures in a GridView", "Chapter 8: Design! Using a DataPicker on a Tablet", "Chapter 9: Customize! Navigating with a Master/Detail Flow Activity on a Tablet", "Chapter 10: Move! Creating Animation", "Chapter 11: Discover! Persistent Data", "Chapter 12: Finale! Publishing Your Android"};
+
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.activity_main, R.id.bootcamp, attraction));
+    }
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        switch (position) {
+            case 0:
+                startActivity(new Intent(MainActivity.this, Chapter1.class));
+                break;
+            case 1:
+                startActivity(new Intent(MainActivity.this, Chapter2.class));
+                break;
+            case 2:
+                startActivity(new Intent(MainActivity.this, Chapter3.class));
+                break;
+            case 3:
+                startActivity(new Intent(MainActivity.this, Chapter4.class));
+                break;
+            case 4:
+                startActivity(new Intent(MainActivity.this, Chapter5.class));
+                break;
+            case 5:
+                startActivity(new Intent(MainActivity.this, Chapter6.class));
+                break;
+            case 6:
+                startActivity(new Intent(MainActivity.this, Chapter7.class));
+                break;
+            case 7:
+                startActivity(new Intent(MainActivity.this, Chapter8.class));
+                break;
+            case 8:
+                startActivity(new Intent(MainActivity.this, Chapter9.class));
+                break;
+            case 9:
+                startActivity(new Intent(MainActivity.this, Chapter10.class));
+                break;
+            case 10:
+                startActivity(new Intent(MainActivity.this, Chapter11.class));
+                break;
+            case 11:
+                startActivity(new Intent(MainActivity.this, Chapter12.class));
+                break;
+        }
     }
 }
